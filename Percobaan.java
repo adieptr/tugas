2590dbfcbe7bdd2cdd19cd8f074d2ecd8b85ae02
adieptr <124704960+adieptr@users.noreply.github.com>
2023-02-08 06:12:28 +0700

    Add files via upload

---
+++ Percobaan.java
@@ -0,0 +1,11 @@
+package gitnyoba;
+
+
+public class Gitnyoba {
+
+    
+    public static void main(String[] args) {
+        System.out.println("Sistem Informasi Berbasis Desktop");
+    }
+    
+}
